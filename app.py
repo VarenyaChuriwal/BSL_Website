7bb198e6e40c2a7a17899e4271ca4160d10c4630
Varenya Churiwal <varenyachuriwal@gmail.com>
2020-07-12 05:01:04 +0530

    Added new routes for Investors tab

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+import csv
+
 from flask import Flask, redirect, jsonify, redirect, render_template
 
 # Configure application
@@ -27,3 +29,27 @@ def man():
 @app.route("/business")
 def business():
     return render_template("business.html")
+
+@app.route("/shareholding")
+def shareholding():
+    return render_template("shareholding.html")
+
+@app.route("/correspondence")
+def correspondence():
+    return render_template("correspondence.html")
+
+@app.route("/policy")
+def policy():
+    return render_template("policy.html")
+
+@app.route("/performance")
+def performance():
+    with open("static/Financial results.csv", "r") as csvfile:
+        rows = csv.reader(csvfile)
+        for row in rows:
+            length = len(row)
+            break
+        csvfile.seek(0)
+        headers = next(rows)
+        return render_template("performance.html", rows=rows, length=length - 3, headers  = headers)
+        
