5efeb0265fb244bd4d36518f00ea2a096d3ed182
Varenya Churiwal <varenyachuriwal@gmail.com>
2020-07-15 03:15:37 +0530

    Added the JS functionality to Entire Manufacturing page

--- static/bsl.js
+++ static/bsl.js
@@ -9,10 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.querySelectorAll(".ref-image").forEach((image) => {
     image.onclick = () => {
-      let ref = image.dataset.reference;
-      document.querySelector(".active").classList.toggle("active");
+      const display =
+        image.parentElement.parentElement.parentElement.previousElementSibling
+          .previousElementSibling.firstElementChild;
+      display.getElementsByClassName("active")[0].classList.toggle("active");
+      const ref = image.dataset.reference;
       document.getElementById(ref).classList.toggle("active");
-      document.getElementById("factory").scrollIntoView();
+      const focus = display.dataset.display;
+      document.getElementById(focus).scrollIntoView();
     };
   });
 });
+
+// document.querySelector(".active").classList.toggle("active");
